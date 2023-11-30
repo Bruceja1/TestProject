c9c891fa846100de8bf218e6785a902631f9268e
Bruce Jagessar <2103932jagessar@zuyd.nl>
2023-11-30 16:35:39 +0100

    MSTest project toegevoegd

---
+++ CalculatorTests_MSTest/CalculatorTests.cs
@@ -0,0 +1,16 @@
+using SimpleCalculator;
+
+namespace CalculatorTests_MSTest
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Addition_InputIs2and5_Returns7()
+        {
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(3, 5);
+            Assert.AreEqual(7, result);
+        }
+    }
+}
