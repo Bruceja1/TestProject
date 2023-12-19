1dab982f48b55e25f4bb4299f602372f9f98a490
Bruce Jagessar <2103932jagessar@zuyd.nl>
2023-12-19 16:17:52 +0100

    Deelfunctie en xUnit test die checkt of correcte error bij nuldeling verschijnt

--- CalculatorTests_xUnit/CalculatorTests.cs
+++ CalculatorTests_xUnit/CalculatorTests.cs
@@ -12,10 +12,15 @@ namespace CalculatorTests_xUnit
         {
 
             Calculator calculator = new Calculator();
-
             int result = calculator.Add(a, b);
-
             Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void Divide_WhenDividedByZero_ThrowsCorrectError()
+        { 
+            Calculator calculator = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calculator.Divide(5,0));
         }
     }
 }
--- SimpleCalculator/Calculator.cs
+++ SimpleCalculator/Calculator.cs
@@ -17,5 +17,23 @@ namespace SimpleCalculator
         {
             return a - b;
         }
+
+        public int Multiply(int a, int b)
+        { 
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException();
+            }
+
+            else
+            {
+                return a / b;
+            }
+        }
     }
 }
